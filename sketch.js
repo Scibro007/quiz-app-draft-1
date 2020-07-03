64ead160746c086312acd6f23d1d9ea5397d151d
Scibro007 <58627849+Scibro007@users.noreply.github.com>
2020-07-03 14:37:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container')
+startButton.addEventListener('dblclick', startGame);
+
+function startGame(){
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    Conso
+}
+
+function nextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
